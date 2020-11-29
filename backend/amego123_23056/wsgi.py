5c3ea740af496bf9b41248ca717a71984d582fbf
Crowdbotics <team@crowdbotics.com>
2020-11-29 14:32:10 +0000

    Initial commit.

---
+++ backend/amego123_23056/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for amego123_23056 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'amego123_23056.settings')
+
+application = get_wsgi_application()
